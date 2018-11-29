ae761aa834c8361df38a6aa9862ba9df0788822c
DeathlyShadowXD <deathlyshadowgamerxd@gmail.com>
2018-11-29 18:54:13 +0000

    full working version for dice rolling

--- bot.js
+++ bot.js
@@ -20,69 +20,89 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', message => {
 	logger.info('user: ' + message.author.id + ' channel: ' + message.channel);
-      var chkString = message.content;
-      var die = '';
-      var multiple = '';
-      var mult_or_die = true;
-      if (chkString[0] === '!')
-      {
-        for (i = 1; i < chkString.length; i++)
+      var content = message.content;
+      if (content.length < 25){
+        if (content[0] === '!')
         {
-          if (typeof chkString[i] == 'number')
+          var reNumDice = /(?:^!)([0-9]+)/;
+          var reDice = /(?:d)([0-9]+)/;
+          var reMod = /[\+\-\*][0-9]+/;
+
+          var numDice;
+          var dice;
+          var mod;
+
+          strDice = reDice.exec(content)
+          if (strDice != null)
           {
-            switch (chkString[i-1])
+            var dice = parseInt(strDice[1], 10);
+            numDice = reNumDice.exec(content);
+            strMod = reMod.exec(content);
+            if (numDice != null)
             {
-            case '!':
-              multiple = chkString[i];
-              mult_or_die = true;
-            break;
-            case 'd':
-              die = chkString[i];
-              mult_or_die = false;
-            break;
-            default:
-              switch (mult_or_die)
-              {
-                case true:
-                  multiple+chkString[i];
-                break;
-                case false:
-                  die+chkString[i];
-                break;
-              }
-            break;
+              var maxI = parseInt(numDice[1], 10);
+            } else
+              maxI = 1;
+            for(var i = 0; i < maxI; i++)
+            {
+            	var rand = math.randomInt(1, dice);
             }
+            if (strMod != null)
+            {
+              var str = strMod[0]
+              switch(str[0])
+              {
+              case '+':
+                logger.info(rand);
+                var int = parseInt(strMod, 10);
+                if (int < 255)
+                  rand += int;
+                logger.info(rand + " " + int);
+              break;
+              case '-':
+                logger.info(rand);
+                int = parseInt(strMod, 10);
+                if (int > -255)
+                  rand += int;
+                logger.info(rand + " " + int);
+              break;
+              case '*':
+                var re = /[0-9]+/;
+                logger.info(rand);
+                finalMod = re.exec(strMod);
+                int = parseInt(finalMod, 10);
+                if (int < 255)
+                  rand *= int;
+                logger.info(rand + " " + int);
+              break;
+              }
+            }
+            logger.info(strDice + " " + maxI + " " + strMod);
+            message.channel.send('dice roll: ' + rand);
           }
+
+
         }
+  			if (message.content === 'hitme')
+  			{
+  				var rand = math.randomInt(2,19809);
 
-        logger.info(multiple + " " + numMultiple);
-        logger.info(die + " " + numDie);
-        var numMultiple = parseInt(multiple, 10);
-        var numDie = parseInt(die, 10);
-
-        var rand = math.randomInt(1,numDie)*numMultiple;
-        message.channel.send('dice roll: ' + rand);
+  				var src;
+  				logger.info('hitme'+' '+rand);
+  				var pageToVisit = "http://herpy.nu/gallery/displayimage.php?pid=" + rand;
+  				request(pageToVisit, function(error, response, body) {
+  				if(response.statusCode === 200) {
+  					var $ = cheerio.load(body);
+  					$('img.image').each(function(i, element){
+  						var src = $(this).attr("src");
+  						logger.info(src);
+  						message.channel.send('Here you go daddy 0w0');
+  						message.channel.send({
+  						files: ['http://herpy.nu/gallery/' + src]});
+  					});
+  				}
+  				});
+  			}
       }
-
-			if (message.content === 'hitme')
-			{
-				var rand = math.randomInt(2,19809);
-
-				var src;
-				logger.info('hitme'+' '+rand);
-				var pageToVisit = "http://herpy.nu/gallery/displayimage.php?pid=" + rand;
-				request(pageToVisit, function(error, response, body) {
-				if(response.statusCode === 200) {
-					var $ = cheerio.load(body);
-					$('img.image').each(function(i, element){
-						var src = $(this).attr("src");
-						logger.info(src);
-						message.channel.send('Here you go daddy 0w0');
-						message.channel.send({
-						files: ['http://herpy.nu/gallery/' + src]});
-					});
-				}
-				});
-			}
 	});
 bot.login(auth.token);
