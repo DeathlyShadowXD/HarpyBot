1e0f5495f89e3c8c9f4bb2217ce4bc5ff8437bba
DeathlyShadowXD <deathlyshadowgamerxd@gmail.com>
2018-11-29 16:43:10 +0000

    added dice rolling functionality

--- bot.js
+++ bot.js
@@ -20,6 +20,47 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', message => {
 	logger.info('user: ' + message.author.id + ' channel: ' + message.channel);
+      var chkString = message.content;
+      var die = '';
+      var multiple = '';
+      var mult_or_die = true;
+      if (chkString[0] === '!')
+      {
+        for (i = 1; i < chkString.length; i++)
+        {
+          if (typeof chkString[i] == 'number')
+          {
+            switch (chkString[i-1])
+            {
+            case '!':
+              multiple = chkString[i];
+              mult_or_die = true;
+            break;
+            case 'd':
+              die = chkString[i];
+              mult_or_die = false;
+            break;
+            default:
+              switch (mult_or_die)
+              {
+                case true:
+                  multiple+chkString[i];
+                break;
+                case false:
+                  die+chkString[i];
+                break;
+              }
+            break;
+            }
+          }
+        }
+        var numMultiple = Integer.parseInt(multiple);
+        var numDie = Integer.parseInt(die);
+
+        var rand = math.randomInt(1,numDie)*numMultiple;
+        message.channel.send('dice roll: ' + rand);
+      }
+
 			if (message.content === 'hitme')
 			{
 				var rand = math.randomInt(2,19809);
